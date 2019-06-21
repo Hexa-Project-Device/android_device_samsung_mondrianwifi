710695563e581e77dee8a0ebb8ccd99c89602064
KangDroid <hyunwoo286@gmail.com>
2019-06-21 22:10:18 +0900

    Its lineage

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_DEVICE := mondrianwifi
 PRODUCT_NAME := lineage_mondrianwifi
